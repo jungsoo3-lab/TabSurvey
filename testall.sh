7d74aa62dd90f5a801be2946238f7d2cb85aa0a1
Jungsoo Park <jungsoopark@wirelessprv-10-192-214-239.near.illinois.edu>
2023-11-02 11:41:51 -0500

    fixed error

--- testall.sh
+++ testall.sh
@@ -39,9 +39,9 @@ MODELS=( #["LinearModel"]=$SKLEARN_ENV
          # ["DANet"]=$TORCH_ENV
           )
 
-CONFIGS=( #"config/adult.yml"
-          #"config/covertype.yml"
-          "config/california_housing.yml"
+CONFIGS=( "config/adult.yml"
+          "config/covertype.yml"
+          #"config/california_housing.yml"
           #"config/higgs.yml"
           )
 
